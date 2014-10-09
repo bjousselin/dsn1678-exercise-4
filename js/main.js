7b02a6c5dba61e8ca4b7a1123cf66c60c40f4b69
bjousselin <benjousselin@Bens-MacBook-Pro.local>
2014-10-09 10:05:19 -0400

    addint shuttle

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var $sun = $('.sun');
 var $sunSection = $('.sun-section');
 var $dipperSection = $('.dipper-section');
 var $dipper = $('.dipper');
+var $shuttle = $('shuttle');
+var $shuttleSection = $('shuttleSection');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -15,3 +17,6 @@ $win.on('scroll', function () {
 $dipperSection.waypoint(function () {
 	$dipper.addClass('js-dipper-fade');
 }, {offset: '50%'});
+
+$shuttleSection.waypoint(function () {
+	$shttle.addclass('js-shuttle-fade');
